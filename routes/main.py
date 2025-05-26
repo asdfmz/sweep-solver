f21d95a1aa69068cbd43eabfb583c1f8e187346f
asdfmz <210786323+asdfmz@users.noreply.github.com>
2025-05-26 20:11:48 +0900

    add: index.html

--- routes/main.py
+++ routes/main.py
@@ -1,7 +1,7 @@
-from flask import Blueprint, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for
 
 bp = Blueprint("main", __name__)
 
 @bp.route("/")
 def root_redirect():
-    return redirect(url_for("init_matrix.size_form"))
+    return render_template("index.html")
